11fb10b4610e8663d30d8ec10f337e837076b061
Giovanni Napoli <gnapoli.dev@gmail.com>
2024-02-05 10:56:36 +0100

    #5833 wip

--- bin/reth/src/commands/dumpgenesis.rs
+++ bin/reth/src/commands/dumpgenesis.rs
@@ -1,10 +1,42 @@
-use clap::Parser;
+//! Command to dump the genesis block configuration to stdout
 
+use crate::{
+    args::utils::{chain_spec_value_parser, SUPPORTED_CHAINS},
+    dirs::{DataDirPath, MaybePlatformPath},
+};
+use clap::Parser;
+use reth_primitives::ChainSpec;
+use std::sync::Arc;
+
+/// Dumps genesis block JSON configuration to stdout
 #[derive(Debug, Parser)]
-pub struct Command {}
+pub struct Command {
+    /// The path to the data dir for all reth files and subdirectories.
+    ///
+    /// Defaults to the OS-specific data directory:
+    ///
+    /// - Linux: `$XDG_DATA_HOME/reth/` or `$HOME/.local/share/reth/`
+    /// - Windows: `{FOLDERID_RoamingAppData}/reth/`
+    /// - macOS: `$HOME/Library/Application Support/reth/`
+    #[arg(long, value_name = "DATA_DIR", verbatim_doc_comment, default_value_t)]
+    datadir: MaybePlatformPath<DataDirPath>,
+
+    /// The chain this node is running.
+    ///
+    /// Possible values are one of the built-in chains.
+    #[arg(
+        long,
+        value_name = "CHAIN_OR_PATH",
+        long_help = format!("Possible value is one of the built-in chains.\nBuilt-in chains:\n {}", SUPPORTED_CHAINS.join(", ")),
+        default_value = SUPPORTED_CHAINS[0],
+        value_parser = chain_spec_value_parser
+    )]
+    chain: Arc<ChainSpec>,
+}
 
 impl Command {
-    pub async fn execute() -> eyre::Result<()> {
-        todo!()
+    /// Execute the `dumpgenesis` command
+    pub async fn execute(&self) -> eyre::Result<()> {
+        Ok(())
     }
 }
